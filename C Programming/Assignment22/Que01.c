2eca86e71500149a3efcc5743f9e26f20b63e1b7
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2024-11-22 13:23:23 -0500

    Assignment 22

---
+++ C Programming/Assignment22/Que01.c
@@ -0,0 +1,46 @@
+/*
+1 2 3 4 
+5 6  7 8
+9 1 2 3
+4 5 6 7
+*/
+#include<stdio.h>
+void Pattern(int iRow ,int iCol)
+{
+    int  i = 0,j = 0;
+    int iNo = 1;
+    
+   for(i=1;i<=iRow;i++)
+   {
+
+      for (j = 1; j <= iCol; j++)
+      {
+        printf("%d\t",iNo);
+        iNo++;
+        
+        if(iNo == 10)
+        {
+            iNo = 1;
+            
+        }
+        
+        
+      }
+      
+       printf("\n");
+   }
+  
+
+}
+int main()
+{
+    int iValue1 = 0,iValue2;
+
+    printf("Enter number of rows and Columns :");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+
+    return 0;
+}
