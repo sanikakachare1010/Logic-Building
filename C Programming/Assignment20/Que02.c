808db5c5045f259712cbb9409d26bb4d94fa0b26
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2024-11-22 23:46:42 +0530

    Assignment20

---
+++ C Programming/Assignment20/Que02.c
@@ -0,0 +1,38 @@
+/*
+     iRow = 4  iCol = 3
+
+     1  2  3
+     1  2  3 
+     1  2  3
+     1  2  3
+*/
+#include<stdio.h>
+void Pattern(int iRow ,int iCol)
+{
+    int  i = 0,j = 0;
+   for(i=1;i<=iRow;i++)
+   {
+
+      for (j = 1; j <= iCol; j++)
+      {
+         printf("%d \t",j);
+
+      }
+      
+       printf("\n");
+   }
+  
+
+}
+int main()
+{
+    int iValue1 = 0,iValue2;
+
+    printf("Enter number of rows and Columns :");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+
+    return 0;
+}
---
+++ C Programming/Assignment20/Que04.c
@@ -0,0 +1,45 @@
+/*
+     iRow = 3  iCol = 5
+
+     *   #   *   #  
+     *   #   *   #  
+     *   #   *   #  
+        
+*/
+#include<stdio.h>
+void Pattern(int iRow ,int iCol)
+{
+    int  i = 0,j = 0;
+   for(i=1;i<=iRow;i++)
+   {
+
+      for (j = iCol; j >= 1; j--)
+      {
+         if(iCol %2 !=0)
+         {
+            printf("*\t");
+         }
+         else
+         {
+            printf("#\t");
+         }
+
+      }
+      
+       printf("\n");
+   }
+  
+
+}
+int main()
+{
+    int iValue1 = 0,iValue2;
+
+    printf("Enter number of rows and Columns :");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+
+    return 0;
+}
