d43d26d1a5331508a6a482e662e777f76ae1f4cd
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2024-12-11 08:18:43 -0500

    Create    Write a recursive program which accept number from user and return its factorial.cpp

---
+++ C Programming/Assignment 42/Write a recursive program which accept number from user and return its factorial.cpp
@@ -0,0 +1,38 @@
+/*
+   Write a recursive program which accept number from user and return its factoria
+   
+*/
+#include<iostream>
+using namespace std;
+
+int Fact(int iNo)
+{
+     static int iFact = 1;
+     
+    if(iNo > 0)
+    {
+       iFact = iFact *iNo;
+       iNo--;
+       
+       Fact(iNo);
+        
+        
+    }
+    return iFact;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Enter number :"<<"\n";
+    cin>>iValue;
+
+    iRet = Fact(iValue);
+
+    cout<<"Factorial is : "<<iRet;
+
+    return 0;
+
+}
