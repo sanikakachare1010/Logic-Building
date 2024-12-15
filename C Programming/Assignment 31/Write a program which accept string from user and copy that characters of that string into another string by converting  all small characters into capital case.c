4d19f5f54d572545eb007ede6884fa655665c80c
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2024-12-15 11:52:27 -0500

    Assignment 31

---
+++ C Programming/Assignment 31/Write a program which accept string from user and copy that characters of that string into another string by converting  all small characters into capital case.c
@@ -0,0 +1,35 @@
+/*
+      Write a program which accept string from user and copy that characters of that string into another string by converting  all small characters into capital case.
+
+*/
+#include<stdio.h>
+void StrCpy(char *src,char *dest)
+{
+    while (*src != '\0')
+    {
+       if(*src >= 'a' && *src <= 'z')
+       {
+              *dest =*src -32;
+       }
+       else
+       {
+             *dest =*src;
+       }
+       src++;
+       dest++;
+    }
+    *dest ='\0';
+    
+}
+int main()
+{
+    char arr[30] = "Marvellous Pythone 2";
+    char brr[30];
+ 
+    StrCpy(arr,brr);
+
+    printf("%s",brr);
+
+    
+    return 0;
+}
